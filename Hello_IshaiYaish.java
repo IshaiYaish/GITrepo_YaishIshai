7dd273f291ce663f25c56b1adc932e3f7bdb8b09
IshaiYaish <ishaiahsi@gmail.com>
2019-09-05 22:52:08 +0300

    Hello World exercise

--- Hello_IshaiYaish.java
+++ Hello_IshaiYaish.java
@@ -1,7 +1,7 @@
 public class Hello_IshaiYaish {
 	public static void main(String[] args) {
 		for(int i=1;i<=10;i++) {
-			System.out.println("Hello world fifth time "+i);
+			System.out.println("Hello world sixth time "+i);
 		}
 	}
 }
